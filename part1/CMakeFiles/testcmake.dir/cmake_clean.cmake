a1681e4062b7adc5c4bc05f5892f065913d976c8
landsea <wobushiludahai@163.com>
2019-12-09 06:46:23 -0800

    cmake start ubuntu

---
+++ part1/CMakeFiles/testcmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testcmake.dir/test.c.o"
+  "testcmake.pdb"
+  "testcmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testcmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

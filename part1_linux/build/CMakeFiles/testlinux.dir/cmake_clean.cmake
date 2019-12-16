899fa6db995ba82a93794809698c33f529dc8c1a
landsea <wobushiludahai@163.com>
2019-12-16 19:31:57 +0800

    update simple linux cmake

---
+++ part1_linux/build/CMakeFiles/testlinux.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testlinux.dir/test.c.o"
+  "testlinux.pdb"
+  "testlinux"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testlinux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
